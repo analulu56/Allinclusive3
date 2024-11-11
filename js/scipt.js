b9a733a5d43dda9ad477c27ffca4b7063173916c
Ana Luiza Gomes Porto <analuiza.gp.veleiros@gmail.com>
2024-11-11 14:16:07 -0300

    update chat

--- js/scipt.js
+++ js/scipt.js
@@ -1,5 +1,50 @@
-// Função para enviar mensagem do usuário
-function sendMessage() {
+const toggleTheme = document.getElementById("toggleTheme");
+
+const rootHtml = document.documentElement;
+
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+
+
+
+//função para alternar o tema 
+function changeTheme () {
+    const currentTheme = rootHtml.getAttribute("data-theme");
+
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
+
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
+
+} 
+
+toggleTheme.addEventListener("click", changeTheme);
+//fim da função mudar o tema
+
+accordionHeaders.forEach( header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove ("active") :
+        accordionItem.classList.add("active");
+    })
+})
+
+
+    // Configuração do Typed.js
+    document.addEventListener("DOMContentLoaded", function () {
+        var typed = new Typed("#typed-text", {
+            strings: ["Preparo", "Oportunidades", "Respeito"],
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 1500,
+            startDelay: 500,
+            loop: true
+        });
+    });
+
+ // Função para enviar mensagem do usuário
+ function sendMessage() {
     var messageInput = document.getElementById('messageInput');
     var message = messageInput.value.trim();
     if (message !== "") {
@@ -47,8 +92,7 @@ function sendCompanyResponse() {
     // Criando a mensagem da empresa
     var messageText = document.createElement('div');
     messageText.classList.add('message-text');
-    messageText.textContent = "Olá! Como posso ajudá-lo?";
-
+    messageText.textContent = "Olá! Como posso ajudá-lo hoje?";
     // Adicionando a imagem e a mensagem da empresa ao chat
     companyMessage.appendChild(companyImg);
     companyMessage.appendChild(messageText);
